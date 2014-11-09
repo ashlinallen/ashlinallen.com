cefd93ed11443aec6c6df6fc63fa03904f0a5ecb
Ashlin Allen <ashlin.allen@gmail.com>
2014-11-08 23:31:53 -0800

    Test JSON parsing

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,35 +1,28 @@
 ﻿using System;
 using System.Net.Mail;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
+using System.Xml;
+using System.Xml.Linq;
+using System.Web.Helpers;
 
 public partial class _Default : System.Web.UI.Page
 {
     [WebMethod, ScriptMethod]
-    public static void SendEmail(NameValue[] namevaluepair)
+    public static void SendEmail(string inJson)
     {
-        StringBuilder builder = new StringBuilder();
         SmtpClient client = new SmtpClient();
         MailAddress from = new MailAddress("contactform@ashlinallen.com");
         MailAddress to = new MailAddress("ashlin.allen@gmail.com");
         MailMessage msg = new MailMessage(from, to);
-
-        foreach(NameValue nvp in namevaluepair)
-        {
-            builder.Append(nvp.Name + ": " + nvp.Value + ";"); 
-        }
+        dynamic json = Json.Decode(inJson);
 
         msg.Subject = "New contact form submission from ashlinallen.com!";
-        msg.Body = HttpContext.Current.Server.HtmlEncode(builder.ToString());
+        msg.Body = HttpContext.Current.Server.HtmlEncode("Company: " + json.Company + " Name" + json.Name);
 
         client.Send(msg);
-    }
-
-    public class NameValue
-    {
-        public String Name;
-        public String Value;
     }
 }
