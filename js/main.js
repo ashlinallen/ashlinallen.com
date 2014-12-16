83f6858b57090d8ff0c97a93bf96bf79d5dc6549
Ashlin Allen <ashlin.allen@gmail.com>
2014-12-15 21:39:42 -0800

    Committing javascript pixel-based scaling version for posterity's sake, but going to switch to a strict responsive version for final implementation. No other solution seems as good.

--- js/main.js
+++ js/main.js
@@ -1043,40 +1043,71 @@
         var centerY = screenHeight / 2,
             centerX = screenWidth / 2,
             length = Math.floor(Math.sqrt(Math.pow(-Math.abs(centerX), 2) + Math.pow(screenHeight - centerY, 2))),
-            earthWidth = parseInt(planetEarth.offsetWidth) - 9;
+            earthWidth = parseInt(planetEarth.offsetWidth) - 9,
+            scale = 1;
 
+        if (topMarginContainer.style.transform !== null) {
+            scale = getScale(topMarginContainer);
+        }
+        
+        length = length * scale;
+        
         earthShadow.style.width = length + "px";
         earthShadow.style.height = earthWidth + "px";
         earthShadow.style.marginTop = -Math.abs(earthWidth / 2) + "px";
     }
 
-    function initObjectSizes() {
-        var width = planetEarth.offsetWidth,
-            pct = Math.floor((planetEarth.offsetWidth / 390) * 100) / 100;
-
-        planetEarth.style.height = planetEarth.offsetWidth + "px";
-        planetEarth.style.marginTop = -Math.abs(width / 2) + "px";
-        planetEarth.style.marginLeft = -Math.abs(width / 2) + "px";
-
-        ash.style.width = (38 * pct) + "px";
-        ash.style.height = (76 * pct) + "px";
-        ash.style.marginLeft = -Math.abs(ash.offsetWidth / 2) + "px";
-        ash.style.marginTop = (-Math.abs((planetEarth.offsetHeight - (planetEarth.offsetHeight * 0.075)) / 2) - ash.offsetHeight) + "px";
+    //function initObjectSizes() {
+    //    var width = planetEarth.offsetWidth,
+    //        pct = Math.floor((planetEarth.offsetWidth / 390) * 100) / 100;
+    //
+    //    planetEarth.style.height = planetEarth.offsetWidth + "px";
+    //    planetEarth.style.marginTop = -Math.abs(width / 2) + "px";
+    //    planetEarth.style.marginLeft = -Math.abs(width / 2) + "px";
+    //
+    //    ash.style.width = (38 * pct) + "px";
+    //    ash.style.height = (76 * pct) + "px";
+    //    ash.style.marginLeft = -Math.abs(ash.offsetWidth / 2) + "px";
+    //    ash.style.marginTop = (-Math.abs((planetEarth.offsetHeight - (planetEarth.offsetHeight * 0.075)) / 2) - ash.offsetHeight) + "px";
+    //    
+    //    moon.style.height = moon.offsetWidth + "px";
+    //    moon.style.marginTop = -Math.abs(moon.offsetWidth / 2) + "px";
+    //    
+    //    lowEarthOrbit.style.height = lowEarthOrbit.offsetWidth + "px";
+    //    lowEarthOrbit.style.marginTop = -Math.abs(lowEarthOrbit.offsetWidth / 2) + "px";
+    //    lowEarthOrbit.style.marginLeft = -Math.abs(lowEarthOrbit.offsetWidth / 2) + "px";
+    //}
+    
+    function initTmcDims() {
+        var tgtEarthPctOfScreen = 0.35,
+            tgtPctInPx = screenWidth * tgtEarthPctOfScreen,
+            tgtScale = (tgtPctInPx / 390).toFixed(3),
+            els = $("")
+                    .add(earthShadow)
+                    .add(ash)
+                    .add(planetEarth)
+                    .add(lowEarthOrbit)
+                    .add(moon);
+                    
+        if (tgtScale > 1.0) { tgtScale = 1.0 };
+        if (tgtScale < 0.5) { tgtScale = 0.5 };
         
-        moon.style.height = moon.offsetWidth + "px";
-        moon.style.marginTop = -Math.abs(moon.offsetWidth / 2) + "px";
-        
-        lowEarthOrbit.style.height = lowEarthOrbit.offsetWidth + "px";
-        lowEarthOrbit.style.marginTop = -Math.abs(lowEarthOrbit.offsetWidth / 2) + "px";
-        lowEarthOrbit.style.marginLeft = -Math.abs(lowEarthOrbit.offsetWidth / 2) + "px";
+        TweenLite.to(topMarginContainer, 0, {
+            css: {
+                scale: tgtScale
+            },
+            ease: Power1.easeInOut
+        });
     }
 
     //Handles screen resize events.
     function resize() {
         resized = true;
+
         updateScreenDims();
+        initTmcDims();
         initShadow();
-        initObjectSizes();
+        //initObjectSizes();
     }
 
     define(requires, function ($) {
@@ -1128,13 +1159,13 @@
         initUADetection();
         preventMobileScale();
 
-        initShadow();
         initStars();
         initInterests();
         initImages();
         initContent();
         initFancybox();
-        initObjectSizes();
+        initTmcDims();
+        initShadow();
 
         meteorShower();
         rotateObjects();
