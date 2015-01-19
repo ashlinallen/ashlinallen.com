4b7eeb9c493b43e44d9aa5159be5a1a977653fe1
Ashlin Allen <ashlin.allen@gmail.com>
2015-01-18 23:45:35 -0800

    Quick hack to fix the contact form for tonight.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,51 +1,29 @@
-﻿using System;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using System.Text;
 using System.Web;
-using System.Web.Script.Serialization;
 using System.Web.Script.Services;
 using System.Web.Services;
 
 public partial class _Default : System.Web.UI.Page
 {
     [WebMethod, ScriptMethod]
-    public static void SendEmail(string name, string email, string note, HttpContext context)
+    public static void SendEmail(string name, string email, string note)
     {
-        //string str;
-        //Context.Response.Clear();
-        //Context.Response.AddHeader("Access-Control-Allow-Origin", "*");
-        //Context.Response.ContentType = "application/json";
-        JavaScriptSerializer js = new JavaScriptSerializer();
+        StringBuilder sbBody = new StringBuilder();
+        SmtpClient client = new SmtpClient();
+        MailAddress from = new MailAddress("contactform@ashlinallen.com");
+        MailAddress to = new MailAddress("ashlin.allen@gmail.com");
+        MailMessage msg = new MailMessage(from, to);
 
-        try {
-            StringBuilder sbBody = new StringBuilder();
-            SmtpClient client = new SmtpClient();
-            MailAddress from = new MailAddress("contactform@ashlinallen.com");
-            MailAddress to = new MailAddress("ashlin.allen@gmail.com");
-            MailMessage msg = new MailMessage(from, to);
+        sbBody.Append("Name: " + name);
+        sbBody.Append("\n");
+        sbBody.Append("Email: " + email);
+        sbBody.Append("\n");
+        sbBody.Append("Note: " + note);
 
-            sbBody.Append("Name: " + name);
-            sbBody.Append("\n");
-            sbBody.Append("Email: " + email);
-            sbBody.Append("\n");
-            sbBody.Append("Note: " + note);
+        msg.Subject = "New contact form submission from ashlinallen.com!";
+        msg.Body = HttpContext.Current.Server.HtmlEncode(sbBody.ToString());
 
-            msg.Subject = "New contact form submission from ashlinallen.com!";
-            msg.Body = HttpContext.Current.Server.HtmlEncode(sbBody.ToString());
-
-            client.Send(msg);
-
-            //str = "{\"status\" : \"success\"}";
-            //Context.Response.Flush(); 
-            //Context.Response.AddHeader("content-length", str.Length.ToString());
-            //Context.Response.Write(str);
-        }
-        catch(Exception ex) 
-        {
-            //str = js.Serialize(ex.Message);
-            //Context.Response.Flush();
-            //Context.Response.AddHeader("content-length", str.Length.ToString());
-            //Context.Response.Write(str);
-        }
+        client.Send(msg);
     }
 }
