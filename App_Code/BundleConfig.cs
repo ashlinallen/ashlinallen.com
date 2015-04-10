2e62f56d89d6a09f7badd0c69f1f771a93e2eabe
Ashlin Allen <ashlin.allen@gmail.com>
2015-04-09 22:29:38 -0700

    Cleaned up a lot of repeated/inaccurate CSS and added any omitted vendor-specific properties, updated code samples

--- App_Code/BundleConfig.cs
+++ App_Code/BundleConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
-using System.Web.UI;
+﻿using System.Web.Optimization;
 using BundleTransformer.Core.Transformers;
 
 namespace ashlinallen.com
